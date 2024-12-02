1c91ea00c58db00eb657e6963fe941268db5a4c9
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2024-12-02 12:39:59 +0300

    add index page

--- local/templates/furnitureAssembly/pages/index/js/index.js
+++ local/templates/furnitureAssembly/pages/index/js/index.js
@@ -6,6 +6,14 @@ $(document).ready( async function() {
         window.Telegram.WebApp.disableVerticalSwipes();
         if(!window.Telegram.WebApp.isVersionAtLeast("8.0")){
             window.Telegram.WebApp.showAlert('Ваша версия телеграма устарела. Пожалуйста, обновите телеграм.');
+        }else{
+            if(window.Telegram.WebApp.initDataUnsafe !== null){
+                const lastName = window.Telegram.WebApp.initDataUnsafe?.user?.last_name || "";
+                const firstName = window.Telegram.WebApp.initDataUnsafe?.user?.first_name || "";
+                const userPhoto = window.Telegram.WebApp.initDataUnsafe?.user?.photo_url || "";
+                $('[userName]').text(lastName + " " + firstName);
+                $('img[userPicture]').attr('src', userPhoto);
+            }
         }
     } catch (e) {
         console.log(e)

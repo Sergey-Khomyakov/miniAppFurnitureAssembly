fb2131d675dedcbb577e1acb3eed9384249cee0e
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2024-12-02 14:04:39 +0300

    init tg name in profile

--- local/templates/furnitureAssembly/pages/profile/js/index.js
+++ local/templates/furnitureAssembly/pages/profile/js/index.js
@@ -1,6 +1,16 @@
 $(document).ready( async function() {
-    window.Telegram.WebApp.BackButton.show();
-    Telegram.WebApp.onEvent('backButtonClicked', function(){
-        window.location.href= "https://sergey-khomyakov.github.io/miniAppFurnitureAssembly/";
-    });
+
+    if(window.Telegram.WebApp.initDataUnsafe !== null){
+        const lastName = window.Telegram.WebApp.initDataUnsafe?.user?.last_name || "";
+        const firstName = window.Telegram.WebApp.initDataUnsafe?.user?.first_name || "";
+        const userPhoto = window.Telegram.WebApp.initDataUnsafe?.user?.photo_url || "";
+        $('[userName]').text(lastName + " " + firstName);
+        $('img[userPicture]').attr('src', userPhoto);
+        
+        window.Telegram.WebApp.BackButton.show();
+        Telegram.WebApp.onEvent('backButtonClicked', function(){
+            window.location.href= "https://sergey-khomyakov.github.io/miniAppFurnitureAssembly/";
+        });
+    }
+
 });

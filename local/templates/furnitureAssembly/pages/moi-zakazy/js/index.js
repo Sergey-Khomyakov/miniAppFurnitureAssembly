6d01edd8a460dd6d9512cdf8a95b64a6b6cc4302
Sergey-Khomyakov <ser.xomiakoff2011@yandex.ru>
2024-12-06 13:20:54 +0300

    update js moi zakazy

--- local/templates/furnitureAssembly/pages/moi-zakazy/js/index.js
+++ local/templates/furnitureAssembly/pages/moi-zakazy/js/index.js
@@ -60,7 +60,7 @@ $(document).ready( async function() {
           duration: 1000
         },
         open: function( event, ui ) {
-            $('body').append('<div id="overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.5);"></div>');
+            $('body').append('<div id="overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100vh; background-color: rgba(0, 0, 0, 0.5);"></div>');
             $('body').css({'overflow-y': 'hidden'});
             $( "#Order" ).css({'margin-top': '-0.5rem'});
         },
